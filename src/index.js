ce6b60c100a3d000e90bbad11034bd78fb441a54
dmitry1992.coding@gmail.com <dmitry1992.coding@gmail.com>
2020-03-09 19:40:08 +0100

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = [];
+    for (let i=0; i < expr.length; i+=10) {
+      arr.push(expr.slice(i, i+ 10));
+    }
+    const arr2 = arr.map(function(elem) {
+      if (elem === '**********') {return ' '};      
+       return elem.substring(elem.search(1), elem.length);
+      }).map(function(elem) {
+        let arr3= [];
+        for (let j=0;j<elem.length;j+=2){
+          arr3.push(elem.slice(j, j+2));          
+        }      
+        return arr3;        
+      });
+     
+    const arr4 = arr2.map(function(elem) {
+    let a = elem.map(function(a) {          
+        if (a === '10') {
+            return '.';
+        }  if (a === '11') {
+            return '-';
+        }  if (a === ' ') {
+            return ' ';
+        }
+         }).join('');        
+        return a;          
+       });
+
+     return arr4.map(a => MORSE_TABLE[a] || a).join('');    
 }
 
 module.exports = {
